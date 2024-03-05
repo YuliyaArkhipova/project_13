a4b7e32fe22c5aaafeffd2eabfe1dbd575a266d5
YuliyaArkhipova <y.arhipova@bk.ru>
2024-03-05 23:43:40 +0300

    add homework 2_2

--- homework13_2/src/classes.py
+++ homework13_2/src/classes.py
@@ -12,22 +12,21 @@ class Category:
         self.__products = products
 
         Category.total_category += 1
-        Category.total_unique_products += len(set(self.products))
+        Category.total_unique_products += len(set(self.__products))
 
     def add_product(self, product):
         """ Метод принимает на вход объект товара и добавляет его в список
      """
         return self.__products.append(product)
 
-
-@property
-def products(self):
-    """Выводит список товаров в формате:
-    Продукт, 80 руб. Остаток: 15 шт.
-    """
-    for i in self.__products:
-        print(f"{i.name}, {i.price}. Остаток: {i.in_stok}")
-    return None
+    @property
+    def products(self):
+        """Выводит список товаров в формате:
+           Продукт, 80 руб. Остаток: 15 шт.
+        """
+        for i in self.__products:
+            print(f"{i.name}, {i.price}. Остаток: {i.in_stok}")
+        return None
 
 
 class Product:
@@ -47,33 +46,26 @@ class Product:
         product = cls(name, description, price, in_stock)
         for i in list_products:
             if product.name == i.name:
-               i.in_stok += product.in_stock
-               if product.price > i.price:
-                i.price = product.price
+                i.in_stok += product.in_stock
+                if product.price > i.price:
+                    i.price = product.price
         else:
-            list_products.append(product)
-        return list_products
+            return product
 
-@property
-def price(self):
-    return self.price
+    @property
+    def price_new(self):
+        return self.price
 
-
-@price.setter
-def prise(self, new_price):
-    """Метод подтверждения пользователем понижения цены
-    """
-    if new_price > 0:
-        if self.price > new_price:
-            user_response = input('Если Вы согласны понизить цену, введите "y"').lower()
-            if user_response == "y":
+    @price_new.setter
+    def price_new(self, new_price):
+        """Метод подтверждения пользователем понижения цены
+        """
+        if new_price > 0:
+            if self.price > new_price:
+                user_response = input('Если Вы согласны понизить цену, введите "y"').lower()
+                if user_response == "y":
+                    self.price = new_price
+            else:
                 self.price = new_price
         else:
-            self.price = new_price
-    else:
-        print('Введена некорректная цена')
-
-
-
-
-
+            print('Введена некорректная цена')
